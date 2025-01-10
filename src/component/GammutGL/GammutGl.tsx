ca0ca123365155b05652a95b56f99bfab5ac110e
spctrm404 <46218112+spctrm404@users.noreply.github.com>
2025-01-10 22:20:39 +0900

    WIP: extending shader

--- src/component/GammutGL/GammutGl.tsx
+++ src/component/GammutGL/GammutGl.tsx
@@ -1,25 +1,114 @@
 import { useEffect, useRef } from 'react';
+import vertex from './vertex.glsl';
+import fragment from './fragment.glsl';
 
-type Axis = 'L' | 'H' | 'C';
+type AxisConfig = 'LC' | 'HC' | 'HL';
 // type Xy = {x:number, y:number};
 type Fx = (input: number) => number;
 
 type GammutGlProps = {
-  axisX: Axis;
-  axisY: Axis;
+  axisConfig: AxisConfig;
   fx: Fx;
+  resolutionMultiplier: number;
 };
 
-const GammutGl = ({ axisX, axisY, fx }: GammutGlProps) => {
+const GammutGl = ({
+  axisConfig,
+  fx,
+  resolutionMultiplier = 1,
+}: GammutGlProps) => {
   const containerRef = useRef<HTMLDivElement>();
   const canvasRef = useRef<HTMLCanvasElement>();
 
   useEffect(() => {
+    const createShader = (
+      gl: WebGL2RenderingContext,
+      type: number,
+      source: string
+    ) => {
+      const shader = gl.createShader(type);
+      if (!shader) return null;
+      gl.shaderSource(shader, source);
+      gl.compileShader(shader);
+      const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+      if (!success) {
+        console.error('Failed to compile shader:', gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return null;
+      }
+      return shader;
+    };
+    const createProgram = (
+      gl: WebGL2RenderingContext,
+      vertexShader: WebGLShader,
+      fragmentShader: WebGLShader
+    ) => {
+      const program = gl.createProgram();
+      if (!program) return null;
+      gl.attachShader(program, vertexShader);
+      gl.attachShader(program, fragmentShader);
+      gl.linkProgram(program);
+      const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+      if (!success) {
+        console.error('Failed to link program:', gl.getProgramInfoLog(program));
+        gl.deleteProgram(program);
+        return null;
+      }
+      return program;
+    };
+
     const container = containerRef.current;
     const canvas = canvasRef.current;
     const gl = canvas?.getContext('webgl2');
     if (!container || !canvas || !gl) return;
-  });
+
+    const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertex);
+    const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragment);
+    if (!vertexShader || !fragmentShader) return;
+
+    const program = createProgram(gl, vertexShader, fragmentShader);
+    if (!program) return;
+
+    gl.useProgram(program);
+    const positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    const vertices = new Float32Array([-1, -1, 1, -1, -1, 1, 1, 1]);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+    const positionAttributeLocation = gl.getAttribLocation(
+      program,
+      'a_position'
+    );
+    gl.enableVertexAttribArray(positionAttributeLocation);
+    gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+
+    const render = () => {
+      gl.clear(gl.COLOR_BUFFER_BIT);
+      gl.uniform2f(
+        gl.getUniformLocation(program, 'u_resolution'),
+        gl.canvas.width,
+        gl.canvas.height
+      );
+      // 변수 삽입
+      gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    };
+
+    const resizeCanvas = () => {
+      const { width, height } = container.getBoundingClientRect();
+      canvas.width = width * resolutionMultiplier;
+      canvas.height = height * resolutionMultiplier;
+      gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+      render();
+    };
+
+    resizeCanvas();
+
+    const resizeObserver = new ResizeObserver(resizeCanvas);
+    resizeObserver.observe(container);
+
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, []);
 };
 
 export default GammutGl;
---
+++ src/component/GammutGL/vertex.glsl
@@ -0,0 +1,6 @@
+// glsl-stage: vertex
+#version 300 es
+in vec4 a_Position;
+void main() {
+  gl_Position = a_Position;
+}
