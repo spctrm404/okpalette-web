49253bef53f1fb708e6e4f0d1ed1c041142ec98f
spctrm404 <46218112+spctrm404@users.noreply.github.com>
2025-02-07 12:33:47 +0900

    SUC: converting lab to rgb and viceversa works well

--- src/oklab/utils.ts
+++ src/oklab/utils.ts
@@ -1,21 +1,9 @@
 import type { Mat3, Vec3 } from "./types";
 
-export const inverseMat3 = (mat3: Mat3): Mat3 | null => {
-  const [[a, d, g], [b, e, h], [c, f, i]] = mat3;
-  const det = a * (e * i - f * h) - b * (d * i - f * g) + c * (d * h - e * g);
-
-  if (det === 0) return null;
-  return [
-    [(e * i - f * h) / det, (c * h - b * i) / det, (b * f - c * e) / det],
-    [(f * g - d * i) / det, (a * i - c * g) / det, (c * d - a * f) / det],
-    [(d * h - e * g) / det, (b * g - a * h) / det, (a * e - b * d) / det],
-  ];
-};
-
 export const multMat3Vec3 = (mat3: Mat3, vec3: Vec3): Vec3 => {
   return [
-    mat3[0][0] * vec3[0] + mat3[1][0] * vec3[1] + mat3[2][0] * vec3[2],
-    mat3[0][1] * vec3[0] + mat3[1][1] * vec3[1] + mat3[2][1] * vec3[2],
-    mat3[0][2] * vec3[0] + mat3[1][2] * vec3[1] + mat3[2][2] * vec3[2],
+    mat3[0][0] * vec3[0] + mat3[0][1] * vec3[1] + mat3[0][2] * vec3[2],
+    mat3[1][0] * vec3[0] + mat3[1][1] * vec3[1] + mat3[1][2] * vec3[2],
+    mat3[2][0] * vec3[0] + mat3[2][1] * vec3[1] + mat3[2][2] * vec3[2],
   ];
 };
--- src/pages/OklabExperiments/OklabExperiments.tsx
+++ src/pages/OklabExperiments/OklabExperiments.tsx
@@ -1,40 +1,115 @@
+import type { Vec3 } from "@/oklab/types";
 import { useCallback, useEffect, useRef, useState } from "react";
+import { srgbToOklch, oklchToSrgb } from "@/oklab/converts";
 import st from "./_OklabExperiments.module.scss";
 import classNames from "classnames/bind";
+import midpoint from "culori/src/easing/midpoint";
 
 const cx = classNames.bind(st);
 
 const OklabExperiments = () => {
-  const [rgb, setRgb] = useState([0, 0, 0]);
+  const [srgb, setSrgb] = useState([0, 0, 0]);
   const [oklch, setOklch] = useState([0, 0, 0]);
+
+  const onChangeSrgb = useCallback(
+    (val: number, idx: number) => {
+      const newSrgb = [...srgb];
+      newSrgb[idx] = val;
+      const newOklch = srgbToOklch(newSrgb as Vec3);
+      setSrgb(newSrgb);
+      setOklch(newOklch);
+    },
+    [srgb],
+  );
+  const onChangeOklch = useCallback(
+    (val: number, idx: number) => {
+      const newOklch = [...oklch];
+      newOklch[idx] = val;
+      const newSrgb = oklchToSrgb(newOklch as Vec3);
+      setOklch(newOklch);
+      setSrgb(newSrgb);
+    },
+    [oklch],
+  );
 
   return (
     <>
-      <input
-        type="number"
-        min={0}
-        max={1}
-        step={0.001}
-        value={rgb[0]}
-        onChange={() => {}}
-      />
-      <input
-        type="number"
-        min={0}
-        max={1}
-        step={0.001}
-        value={rgb[1]}
-        onChange={() => {}}
-      />
-      <input
-        type="number"
-        min={0}
-        max={1}
-        step={0.001}
-        value={rgb[2]}
-        onChange={() => {}}
-      />
-      <div></div>
+      <div className={cx("color-picker", "color-picker-srgb")}>
+        <input
+          type="number"
+          min={0}
+          max={1}
+          step={0.001}
+          value={srgb[0]}
+          onChange={(e) => {
+            onChangeSrgb(parseFloat(e.target.value), 0);
+          }}
+        />
+        <input
+          type="number"
+          min={0}
+          max={1}
+          step={0.001}
+          value={srgb[1]}
+          onChange={(e) => {
+            onChangeSrgb(parseFloat(e.target.value), 1);
+          }}
+        />
+        <input
+          type="number"
+          min={0}
+          max={1}
+          step={0.001}
+          value={srgb[2]}
+          onChange={(e) => {
+            onChangeSrgb(parseFloat(e.target.value), 2);
+          }}
+        />
+        <div
+          className={cx("swatch")}
+          style={{
+            backgroundColor: `color(srgb ${Math.min(Math.max(srgb[0], 0), 1)} ${Math.min(Math.max(srgb[1], 0), 1)} ${Math.min(Math.max(srgb[2], 0), 1)})`,
+          }}
+        ></div>
+      </div>
+      <div className={cx("color-picker", "color-picker-oklch")}>
+        <input
+          type="number"
+          min={0}
+          max={1}
+          step={0.001}
+          value={oklch[0]}
+          onChange={(e) => {
+            onChangeOklch(parseFloat(e.target.value), 0);
+          }}
+        />
+        <input
+          type="number"
+          min={0}
+          max={1}
+          step={0.001}
+          value={oklch[1]}
+          onChange={(e) => {
+            onChangeOklch(parseFloat(e.target.value), 1);
+          }}
+        />
+        <input
+          type="number"
+          min={0}
+          max={360}
+          step={1}
+          value={oklch[2]}
+          onChange={(e) => {
+            onChangeOklch(parseFloat(e.target.value), 2);
+          }}
+        />
+        <div
+          className={cx("swatch")}
+          style={{
+            backgroundColor: `oklch(${oklch[0]} ${oklch[1]} ${oklch[2]}deg)`,
+          }}
+        ></div>
+      </div>
     </>
   );
 };
