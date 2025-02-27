bfe7552566b0a0a626cdb3febfd67e1a9f7e160d
spctrm404 <46218112+spctrm404@users.noreply.github.com>
2025-02-27 23:47:50 +0900

    SUC: XYSlider; touch-action applied

--- src/components/XYSlider/XYThumb/XYThumb.tsx
+++ src/components/XYSlider/XYThumb/XYThumb.tsx
@@ -1,18 +1,20 @@
-// todo: touch situ
-
 import type { Dim2D } from "../index";
 import { useContext, useEffect, useMemo, useRef, useState } from "react";
 import { XYTrackContext } from "../XYTrack";
 import { mergeProps, useFocus, useHover, useMove, usePress } from "react-aria";
+import st from "./_XYThumb.module.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(st);
 
 export type XYThumbProps = {
   min?: Dim2D;
   max?: Dim2D;
   step?: Dim2D;
   val: Dim2D;
+  idx?: number;
   onChange?: (newVal: Dim2D) => void;
   constraintVal?: (val: Dim2D) => Dim2D;
-  index?: number;
 };
 
 export const XYThumb = ({
@@ -20,9 +22,9 @@ export const XYThumb = ({
   max = { x: 100, y: 100 },
   step = { x: 1, y: 1 },
   val,
+  idx,
   onChange,
   constraintVal,
-  index,
 }: XYThumbProps) => {
   const { trackSize, thumbSize } = useContext(XYTrackContext);
 
@@ -142,7 +144,7 @@ export const XYThumb = ({
       pxPos.y += e.deltaY;
 
       moveCommon(pxPos);
-      console.log(`${index}: onMove`);
+      console.log(`${idx}: onMove`);
     },
     onMoveEnd: () => {
       // update pxPos
@@ -151,24 +153,24 @@ export const XYThumb = ({
 
       moveCommon(pxPosition);
       isMovingRef.current = false;
-      console.log(`${index}: onMoveEnd`);
+      console.log(`${idx}: onMoveEnd`);
     },
   });
   const { pressProps, isPressed } = usePress({
     onPress: () => {
-      // console.log(`${index}: onPress`);
+      // console.log(`${idx}: onPress`);
     },
     onPressStart: () => {
-      // console.log(`${index}: onPressStart`);
+      // console.log(`${idx}: onPressStart`);
     },
     onPressEnd: () => {
-      // console.log(`${index}: onPressEnd`);
+      // console.log(`${idx}: onPressEnd`);
     },
     onPressChange: () => {
-      // console.log(`${index}: onPressChange`);
+      // console.log(`${idx}: onPressChange`);
     },
     onPressUp: () => {
-      // console.log(`${index}: onPressUp`);
+      // console.log(`${idx}: onPressUp`);
     },
   });
 
@@ -181,11 +183,11 @@ export const XYThumb = ({
 
   // update position from value
   if (val.x !== lastValRef.current.x || val.y !== lastValRef.current.y) {
-    console.log(`${index}: new value come`);
+    console.log(`${idx}: new value come`);
     if (constraintVal) {
       const constrainedVal = constraintVal(val);
       if (constrainedVal.x !== val.x || constrainedVal.y !== val.y) {
-        console.log(`${index}: value needs to be constrained`);
+        console.log(`${idx}: value needs to be constrained`);
         onChange?.(constrainedVal);
       }
     }
@@ -194,7 +196,7 @@ export const XYThumb = ({
       (pxPosFromVal.x !== pxPosRef.current.x ||
         pxPosFromVal.y !== pxPosRef.current.y)
     ) {
-      console.log(`${index}: val -> pos`);
+      console.log(`${idx}: val -> pos`);
       pxPosRef.current = pxPosFromVal;
       setnormPos(normPosFromVal);
     }
@@ -208,28 +210,17 @@ export const XYThumb = ({
   return (
     <>
       <div
+        className={cx("xy-thumb")}
         {...reactAriaProps}
-        tabIndex={0}
+        tabIndex={idx}
         style={{
           width: thumbSize.width,
           height: thumbSize.height,
-          borderRadius: "100%",
           position: "absolute",
           left: `calc(${100 * normPos.x}% - ${0.5 * thumbSize.width}px)`,
           bottom: `calc(${100 * normPos.y}% - ${0.5 * thumbSize.height}px)`,
-          background: "black",
         }}
       />
-      <div
-        style={{
-          position: "absolute",
-          left: `calc(${100 * normPos.x}% - ${0.5 * thumbSize.width}px)`,
-          top: `calc(${100 * (1 - normPos.y)}% - ${0.5 * thumbSize.height}px + 24px)`,
-        }}
-      >
-        <div>{`${normPos.x}, ${normPos.y}`}</div>
-        <div>{`${pxPosRef.current.x}, ${pxPosRef.current.y}`}</div>
-      </div>
     </>
   );
 };
